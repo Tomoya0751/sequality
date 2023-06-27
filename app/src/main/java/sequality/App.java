eefd330543af962a7cd7a2241bd59138273f4056
Tomoya0751 <e1c20017@oit.ac.jp>
2023-06-27 09:05:22 +0900

    output of app

--- app/src/main/java/sequality/App.java
+++ app/src/main/java/sequality/App.java
@@ -10,5 +10,12 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        int sum = new Calculate().sum(2, 3);
+        double ave = new Calculate().average(sum, 2);
+        System.out.println("Sum of 2 and 3 is " + sum + "." + "Average is " + ave + ".");
+        sum = new Calculate().sumOfRange(1, 10);
+        ave = new Calculate().average(sum, 10);
+        System.out.println("Sum of 1 to 10 is " + sum + "." + "Average is " + ave + ".");
+        System.out.println("Sum of odd of 1 to 10 is " + new Calculate().sumOfOdd(1, 10) + "." + "Sum of even is " + new Calculate().sumOfEven(1, 10) + ".");
     }
 }
--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -4,4 +4,36 @@ public class Calculate {
     public int sum(int x, int y){
         return x + y;
     }
+
+    public double average(int sum, int num){
+        return (double)sum / num;
+    }
+
+    public int sumOfRange(int start, int end){
+        int total = 0;
+        for(int i = start; i <= end; i++){
+            total += i;
+        }
+        return total;
+    }
+
+    public int sumOfOdd(int start, int end){
+        int total = 0;
+        for(int i = start; i <= end; i++){
+            if(i % 2 != 0){
+                total += i;
+            }
+        }
+        return total;
+    }
+
+    public int sumOfEven(int start, int end){
+        int total = 0;
+        for(int i = start; i <= end; i++){
+            if(i % 2 == 0){
+                total += i;
+            }
+        }
+        return total;
+    }
 }
